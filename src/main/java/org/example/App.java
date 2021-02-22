2a50da6045987d724ebbf6c18243cf0142c69408
Ahmad-Abdou <kyoub1996@gmail.com>
2021-02-22 11:26:16 +0100

    fixing projects

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,9 +13,9 @@ public class App {
     public static void main(String[] args){
 
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-    StudentDao dao = context.getBean("userAppDao", StudentDao.class);
+    StudentDao dao = context.getBean("student", StudentDao.class);
     Student createdStudent = dao.saveOrUpdate(new Student("test"));
-        System.out.println("createdAppUser = "+createdStudent);
+        System.out.println("createdStudent = "+createdStudent);
 
   }
 }
