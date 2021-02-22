63de971043d453b1ec1bf33b63fd31453a09199b
Ahmad-Abdou <kyoub1996@gmail.com>
2021-02-22 11:28:08 +0100

    Done Workshop

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,15 +5,12 @@ import org.example.dao.StudentDao;
 import org.example.model.Student;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
     public static void main(String[] args){
 
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-    StudentDao dao = context.getBean("student", StudentDao.class);
+    StudentDao dao = context.getBean("StudentDao", StudentDao.class);
     Student createdStudent = dao.saveOrUpdate(new Student("test"));
         System.out.println("createdStudent = "+createdStudent);
 
